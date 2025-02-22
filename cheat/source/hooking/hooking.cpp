9556c997a4efcc89a4051a242e2b97abfc0b22e5
Eve <hi@sipacid.com>
2025-02-22 14:56:08 +0100

    fixed failed detour hooking

--- cheat/source/hooking/hooking.cpp
+++ cheat/source/hooking/hooking.cpp
@@ -4,9 +4,6 @@ using namespace Asthmaphobia;
 
 Hooking::Hooking()
 {
-	DetourTransactionBegin();
-	DetourUpdateThread(GetCurrentThread());
-
 	hooking = this;
 }
 
@@ -25,21 +22,48 @@ void Hooking::AddHook(const std::string& functionName, PVOID* originalFunction, 
 
 void Hooking::ApplyHooks() const
 {
+	if (DetourTransactionBegin() != NO_ERROR)
+	{
+		throw std::runtime_error("Failed to begin detour transaction");
+	}
+
+	if (DetourUpdateThread(GetCurrentThread()) != NO_ERROR)
+	{
+		throw std::runtime_error("Failed to update detour thread");
+	}
+
 	for (const auto& hook : this->hooks_)
 	{
 		std::ostringstream oss;
 		oss << "Applying hook: " << std::get<0>(hook) << "\n";
 		LOG_DEBUG(oss.str());
 
-		DetourAttach(std::get<1>(hook), std::get<2>(hook));
+		const LONG error = DetourAttach(std::get<1>(hook), std::get<2>(hook));
+		if (error != NO_ERROR)
+		{
+			std::ostringstream oss;
+			oss << "Failed to attach hook: " << std::get<0>(hook) << " with error code: " << error << "\n";
+			LOG_ERROR(oss.str());
+		}
 	}
-	DetourTransactionCommit();
+
+	if (DetourTransactionCommit() != NO_ERROR)
+	{
+		throw std::runtime_error("Failed to commit detour transaction");
+	}
 }
 
 void Hooking::RemoveHooks() const
 {
-	DetourTransactionBegin();
-	DetourUpdateThread(GetCurrentThread());
+	if (DetourTransactionBegin() != NO_ERROR)
+	{
+		throw std::runtime_error("Failed to begin detour transaction");
+	}
+
+	if (DetourUpdateThread(GetCurrentThread()) != NO_ERROR)
+	{
+		throw std::runtime_error("Failed to update detour thread");
+	}
 
 	for (const auto& hook : this->hooks_)
 	{
@@ -47,7 +71,17 @@ void Hooking::RemoveHooks() const
 		oss << "Restoring hook: " << std::get<0>(hook) << "\n";
 		LOG_DEBUG(oss.str());
 
-		DetourDetach(std::get<1>(hook), std::get<2>(hook));
+		const LONG error = DetourDetach(std::get<1>(hook), std::get<2>(hook));
+		if (error != NO_ERROR)
+		{
+			std::ostringstream oss;
+			oss << "Failed to detach hook: " << std::get<0>(hook) << " with error code: " << error << "\n";
+			LOG_ERROR(oss.str());
+		}
 	}
-	DetourTransactionCommit();
+
+	if (DetourTransactionCommit() != NO_ERROR)
+	{
+		throw std::runtime_error("Failed to commit detour transaction");
+	}
 }
