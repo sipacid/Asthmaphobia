02043b045299d43be05fe9ebf4bda2d88c0b2d0c
Eve <hi@sipacid.com>
2025-03-14 19:35:02 +0100

    new website who dis

---
+++ website/src/config/appInfo.ts
@@ -0,0 +1,17 @@
+/**
+ * Central configuration file for Asthmaphobia application information
+ * Update these values when releasing new versions
+ */
+
+export const appInfo = {
+  name: "Asthmaphobia",
+  version: "2.4.1",
+  latestUpdateDate: "2025-03-14",
+  copyrightYear: "2025",
+  downloadUrl: "https://asthmaphobia.xyz/Asthmaphobia.rar",
+  socialLinks: {
+    github: "https://github.com/sipacid/asthmaphobia-issues",
+    signal:
+      "https://signal.group/#CjQKIPxbTnnAqjtjLb53VsJKD--hx0o2s6zkfnsNEFcJok_NEhCP3xrE_Et5kwfaHK0Yu8Ns",
+  },
+};
