a6c283b9e19a85a5cd447422b5250a3d5c476832
Eve <hi@sipacid.com>
2025-03-14 19:35:26 +0100

    removed nextjs version

--- website-del/data/auth.tsx
+++
@@ -1,8 +0,0 @@
-import { decrypt } from "@/app/lib/session";
-import { cookies } from "next/headers";
-import { cache } from "react";
-
-export const verifySession = cache(async () => {
-  const cookie = (await cookies()).get("session")?.value;
-  return await decrypt(cookie);
-});
